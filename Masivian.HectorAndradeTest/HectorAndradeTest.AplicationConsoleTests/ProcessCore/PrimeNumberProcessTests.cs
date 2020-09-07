4db67f275450345f2072b7aed40c885a0cb7ea43
HectorAndrade88 <38307901+HectorAndrade88@users.noreply.github.com>
2020-09-07 03:19:45 -0500

    Unit Test
    Add proyect Unit Test

---
+++ Masivian.HectorAndradeTest/HectorAndradeTest.AplicationConsoleTests/ProcessCore/PrimeNumberProcessTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HectorAndradeTest.AplicationConsole.ProcessCore.Tests
+{
+    [TestClass()]
+    public class PrimeNumberProcessTests
+    {
+        [TestMethod()]
+        public void True_IsNumberPrimeTest()
+        {
+            bool result = PrimeNumberProcess.IsNumberPrime(1);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void False_IsNumberPrimeTest()
+        {
+            bool result = PrimeNumberProcess.IsNumberPrime(8);
+            Assert.IsFalse(result);
+        }
+    }
+}
